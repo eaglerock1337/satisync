cc604f9e1e3fffea8af9463346693efb5a739fce
Peter Marks <peter.marks@gmail.com>
2019-10-13 21:46:59 -0400

    lol I got far

--- satisync.py
+++ satisync.py
@@ -5,6 +5,13 @@ satisync.py - Probably not a good idea
 But we're doing it anyway.
 """
 
+class Satisync:
+    """
+    I'll find a good excuse for this later.
+    """
+    def __init__(self):
+        pass
+
 def do_the_thing():
     pass
 
